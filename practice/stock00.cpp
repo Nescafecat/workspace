bbabdffab81cd88da3922bfc65eef4ab1bc71dfa
nescafecat <cdxrcq@gmail.com>
2018-05-01 21:11:42 +0800

    consturct

--- practice/stock00.cpp
+++ practice/stock00.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
 #include"stock00.h"
 
-void Stock::acquire(const std::string & co, long n, double pr)
+Stock::Stock()
 {
+    std::cout << "Default constructor called\n";
+    company = "no name";
+    shares = 0;
+    share_val = 0.0;
+    total_val = 0.0;
+}
+
+Stock::Stock(const std::string & co, long n, double pr)
+{
+    std::cout << "Constructor using " << co << "called\n";
     company = co;
     if (n < 0)
     {
@@ -14,6 +24,11 @@ void Stock::acquire(const std::string & co, long n, double pr)
         shares = n;
     share_val = pr;
     set_tot();
+}
+
+Stock::~Stock()
+{
+    std::cout << "Bye, " << company << "!\n";
 }
 
 void Stock::buy(long num, double price)
--- practice/stock00.h
+++ practice/stock00.h
@@ -12,7 +12,9 @@ class Stock
         double total_val;
         void set_tot(){ total_val = shares *share_val; }
     public:
-        void acquire(const std::string & co, long n, double pr);
+        Stock();
+        Stock(const std::string & co, long n = 0, double pr = 0.0);
+        ~Stock();
         void buy(long num, double price);
         void sell(long num, double price);
         void update(double price);
